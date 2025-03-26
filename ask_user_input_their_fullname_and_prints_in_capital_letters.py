ec29688d1d0417833422132f56dfb83494f37219
talamor-shella <shellatalamor@gmail.com>
2025-03-26 19:00:41 +0800

    AddedPseudocode

--- ask_user_input_their_fullname_and_prints_in_capital_letters.py
+++ ask_user_input_their_fullname_and_prints_in_capital_letters.py
@@ -1,1 +1,4 @@
 #Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+#ask user input with upper() method
+#prints the user input in capital case
