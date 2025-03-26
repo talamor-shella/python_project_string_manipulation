47131bfff0a054d3cb21793d6ca605f4296ea4ea
talamor-shella <shellatalamor@gmail.com>
2025-03-26 18:52:58 +0800

    ask user input a number

--- ask_user_input_a_number_0_1000_prints_the_number_in_6_digit_format.py
+++ ask_user_input_a_number_0_1000_prints_the_number_in_6_digit_format.py
@@ -1,5 +1,7 @@
 #Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
 #ask user input
+number = input("Enter a number: ")
+
 #use zfill method
 #print the user input 
