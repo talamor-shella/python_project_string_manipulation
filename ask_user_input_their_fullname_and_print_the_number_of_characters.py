8999304d657c6d8ac2b1b3d5d7c26ec5435c3087
talamor-shella <shellatalamor@gmail.com>
2025-03-26 20:11:20 +0800

    AddedPseudocode

--- ask_user_input_their_fullname_and_print_the_number_of_characters.py
+++ ask_user_input_their_fullname_and_print_the_number_of_characters.py
@@ -1,1 +1,5 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+#ask user input and use strip method to remove spaces
+#len() to return number of characters
+#prints the number of characters
