df04868e7010bc816ce4209629ec44d25fa7dfea
talamor-shella <shellatalamor@gmail.com>
2025-03-26 20:20:50 +0800

    ask user input and uses title method to for proper casing

--- ask_user_input_fullname_in_incorrect_casing_and_prints_the_input_in_pascal_case.py
+++ ask_user_input_fullname_in_incorrect_casing_and_prints_the_input_in_pascal_case.py
@@ -1,5 +1,7 @@
 #Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 #ask user input with title() method
+fullname = input("Enter your fullname: ").title()
+
 #use replace method to remove the spaces
 #prints the input in pascal case
