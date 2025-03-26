3d6901917c6713757cc6a766674c8e6afa5019ec
talamor-shella <shellatalamor@gmail.com>
2025-03-26 19:01:43 +0800

    ask user input and uses upper method to capitalize the letters

--- ask_user_input_their_fullname_and_prints_in_capital_letters.py
+++ ask_user_input_their_fullname_and_prints_in_capital_letters.py
@@ -1,4 +1,6 @@
 #Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 #ask user input with upper() method
+fullname = input("Enter your fullname: ").upper()
+
 #prints the user input in capital case
