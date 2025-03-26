60849cda138306439112fa4e2bcc762d3e74ecd9
talamor-shella <shellatalamor@gmail.com>
2025-03-26 19:13:26 +0800

    ask user input and uses title method for proper casing

--- ask_user_input_their_fullname_in_incorrect_casing_and_prints_in_proper_casing.py
+++ ask_user_input_their_fullname_in_incorrect_casing_and_prints_in_proper_casing.py
@@ -1,4 +1,6 @@
 #Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
 #ask user input and use title() method
+fullname = input("Enter your fullname: ").title()
+
 #prints the user input in proper case
