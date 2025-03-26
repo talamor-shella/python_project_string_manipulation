978dff6ad18e1f3dd6f026c51d8254cc840f3d2b
talamor-shella <shellatalamor@gmail.com>
2025-03-26 20:03:31 +0800

    AddedPseudocode

--- ask_user_input_complete_statement_prints_the_number_of_words.py
+++ ask_user_input_complete_statement_prints_the_number_of_words.py
@@ -1,2 +1,5 @@
 #Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
 
+#ask user input and use split method()
+#len() method to return the number of words
+#prints the number of words
