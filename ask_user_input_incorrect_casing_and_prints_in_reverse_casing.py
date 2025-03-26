d5e2f082ebcd344815abb2b0a5234367e9fcf008
talamor-shella <shellatalamor@gmail.com>
2025-03-26 19:57:12 +0800

    ask user input and uses swapcase() method to reverse casing

--- ask_user_input_incorrect_casing_and_prints_in_reverse_casing.py
+++ ask_user_input_incorrect_casing_and_prints_in_reverse_casing.py
@@ -1,4 +1,6 @@
 #Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 #ask user input with swapcase() method
+fullname = input("Enter your fullname: ").swapcase()
+
 #prints in reverse case 
