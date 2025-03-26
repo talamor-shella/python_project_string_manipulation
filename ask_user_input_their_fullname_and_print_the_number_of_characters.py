344010b55667b86b7900ffcc86f922fc2ed1ef92
talamor-shella <shellatalamor@gmail.com>
2025-03-26 20:12:13 +0800

    ask user input and uses strip method to remove spaces

--- ask_user_input_their_fullname_and_print_the_number_of_characters.py
+++ ask_user_input_their_fullname_and_print_the_number_of_characters.py
@@ -1,5 +1,7 @@
 #Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 #ask user input and use strip method to remove spaces
+fullname = input("Enter your fullname: ").strip()
+
 #len() to return number of characters
 #prints the number of characters
